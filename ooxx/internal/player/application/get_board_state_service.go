01da7644f9e01e1c2afb4e75453951f4118a9312
nimo_wang <nimo_wang@paradise-soft.com.tw>
2023-03-01 11:06:28 +0800

    fix lint

--- ooxx/internal/player/application/get_board_state_service.go
+++ ooxx/internal/player/application/get_board_state_service.go
@@ -19,16 +19,5 @@ func NewGetBoardState(boardPort out.IBoardPort) in.IGetBoardStateUseCase {
 
 func (gbs *getBoardState) GetBoardState() ([3][3]domain.State, error) {
 	tmpB, err := gbs.boardPort.GetBoardState()
-	// 不在這裡轉換，那是adapter該做的事情
-	// if err != nil {
-	// 	return [3][3]domain.State{}, errors.Wrap(err, "in service resetBoardState")
-	// }
-
-	// for i := range tmpB {
-	// 	for j := range tmpB[i] {
-	// 		b[i][j] = domain.State(tmpB[i][j])
-	// 	}
-	// }
-
 	return tmpB, errors.Wrap(err, "in GetBoardState")
 }
